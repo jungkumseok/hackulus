1b28cfddb408b16af7402b479f92b8ec9b5bff63
Kumseok Jung <jungkumseok@gmail.com>
2016-02-12 00:32:46 -0800

    Modified host address

--- hackulus/config.py
+++ hackulus/config.py
@@ -1,4 +1,4 @@
 APP_CONFIG = {
-              'host': 'http://192.168.0.73:8000',
+              'host': 'http://127.0.0.1:8000',
               'libs_url': 'http://src.jungkumseok.com/'
               }
